2d306aa5b3ccbae1cfd57281b11effcd59e223c3
var_rishabh <right.rishabh@gmail.com>
2023-04-21 23:37:08 +0530

    env setup complete

--- server/server.js
+++ server/server.js
@@ -1,28 +1,32 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const dotenv = require("dotenv");
 const colors = require("colors");
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 dotenv.config();
+
+const connectDB = require("./config/db");
+connectDB();
 
 app.get("/", async (req, res) => {
   try {
-    return res
-      .status(200)
-      .json({
-        status: "success",
-        message: "API running successfully",
-        data: null,
-      });
+    return res.status(200).json({
+      status: "success",
+      message: "API running successfully",
+      data: null,
+    });
   } catch (err) {
-    return res
-      .status(401)
-      .json({
-        status: "failure",
-        message: "API not running successfully",
-        data: null,
-      });
+    return res.status(401).json({
+      status: "failure",
+      message: "API not running successfully",
+      data: null,
+    });
   }
 });
 
@@ -30,5 +34,5 @@ app.listen(process.env.PORT, (err) => {
   if (err) {
     console.log(`Error: ${err}`.red);
   }
-  console.log(`Server running on PORT ${process.env.PORT}`.yellow.underline);
+  console.log(`Server running on PORT ${process.env.PORT}`.blue.underline);
 });
