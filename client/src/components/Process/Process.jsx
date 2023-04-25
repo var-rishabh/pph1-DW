9bef5e24be23d4eb193acb79c728fce5e0d6d37c
Shubham Aggarwal <89722744+Shubham-Aggarwal-1306@users.noreply.github.com>
2023-04-26 01:29:22 +0530

    Routing and Basic Color Pallete

---
+++ client/src/components/Process/Process.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import "./Process.css";
+const Process = () => {
+  return (
+    <div>Process</div>
+  )
+}
+
+export default Process;
---
+++ client/src/components/Product/ProductCatalogue.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import "./ProductCatalogue.css";
+const ProductCatalogue = () => {
+  return (
+    <div>ProductCatalogue</div>
+  )
+}
+
+export default ProductCatalogue;
