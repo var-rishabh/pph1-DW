c9ce14c9e591ad5b5f41d629c2808598f752ddd8
Shubham Aggarwal <89722744+Shubham-Aggarwal-1306@users.noreply.github.com>
2023-04-26 01:29:22 +0530

    Routing and Basic Color Pallete

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,31 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useDispatch, useSelector } from 'react-redux';
-import Landing from './components/Landing/Landing';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import ProductCatalogue from './components/Product/ProductCatalogue';
+import Product from './components/Product/Product';
+import Process from './components/Process/Process';
+import Order from './components/Order/Order';
+import Checkout from './components/Checkout/Checkout';
+import Gallery from './components/Gallery/Gallery';
 import Login from './components/Login/Login';
 import Register from "./components/Register/Register";
+
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={ <Landing />} />
+        <Route path="/" element={ <Home />} />
+        <Route path="/about" element={<About/>} />
         <Route path="/login" element= {<Login/>} />
         <Route path="/register" element={<Register/>}/>
+        <Route path="/product" element={<ProductCatalogue/>} />
+        <Route path="/product/:id" element={<Product/>} />
+        <Route path="/process" element={<Process/>} />
+        <Route path="/order" element={<Order/>} />
+        <Route path="/checkout" element={<Checkout/>} />
+        <Route path="/gallery" element={<Gallery/>} />
       </Routes>
     </Router>
   );
