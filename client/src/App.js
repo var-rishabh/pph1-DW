e1637bc194397e4581682f6f8cae7a1e643e849a
Shubham Aggarwal <89722744+Shubham-Aggarwal-1306@users.noreply.github.com>
2023-04-25 18:31:54 +0530

    Redux Template and Routing Started

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux';
+import Landing from './components/Landing/Landing';
+import Login from './components/Login/Login';
+import Register from "./components/Register/Register";
 function App() {
   return (
-    <div className="App">
-      My React App
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={ <Landing />} />
+        <Route path="/login" element= {<Login/>} />
+        <Route path="/register" element={<Register/>}/>
+      </Routes>
+    </Router>
   );
 }
 
