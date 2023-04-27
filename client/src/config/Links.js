123d4f8a3d9cc3447b7a28f514796a96b34873f9
Shubham Aggarwal <89722744+Shubham-Aggarwal-1306@users.noreply.github.com>
2023-04-27 23:09:05 +0530

    FooterMain and FooterSub

--- client/src/config/Links.js
+++ client/src/config/Links.js
@@ -1,6 +1,7 @@
 const Links = {
     facebook: 'https://www.facebook.com/your-facebook-page',
     instagram: 'https://www.instagram.com/your-instagram-page',
+    playstore: 'https://play.google.com/store/apps/details?id=com.apkadoodhwala',
 }   
 
 export default Links;
