4807134c9703e80be672b7c686299b2076c00640
Shubham Aggarwal <89722744+Shubham-Aggarwal-1306@users.noreply.github.com>
2023-04-27 23:09:05 +0530

    FooterMain and FooterSub

--- client/src/components/Footer/FooterMain.jsx
+++ client/src/components/Footer/FooterMain.jsx
@@ -1,10 +1,64 @@
 import React from 'react'
 import "./FooterMain.css";
+import whiteIcon from "../../Assets/whiteIcon.png";
 
 const FooterMain = () => {
   return (
-    <div>FooterMain</div>
+    <div className="footer-main">
+      <div className="footer-main__description">
+        <div className="footer-main__description--icon">
+          <img src={whiteIcon} alt="logo" />
+        </div>
+        <div className="footer-main__description--text">
+          Every single day, we at Happy Nature endeavour to provide
+          you and your family with the healthiest,
+          tastiest and freshest milk produced in a cruelty –
+          free, hygienic and modern dairy farm. With each drop that you drink,
+          you fall in love with milk all over again!
+        </div>
+      </div>
+      <div className="footer-main__links">
+        <div className="footer-main--title">
+          Useful Links
+        </div>
+        <div className="footer-main__links--list">
+          <div className="footer-main__links--list--item">
+            <a href="/">Home</a>
+          </div>
+          <div className="footer-main__links--list--item">
+            <a href="/about">About</a>
+          </div>
+          <div className="footer-main__links--list--item">
+            <a href="/product">Product</a>
+          </div>
+          <div className="footer-main__links--list--item">
+            <a href="/farms">Our Farms</a>
+          </div>
+          <div className="footer-main__links--list--item">
+            <a href="/blog">Blogs</a>
+          </div>
+        </div>
+      </div>
+      <div className="footer-main__contact">
+        <div className="footer-main--title">
+          Contact Us
+        </div>
+        <div className="footer-main__contact--list">
+          <div className="footer-main__contact--list--item">
+            Call us - 18005720711
+          </div>
+          <div className="footer-main__contact--list--item">
+            Email - support@apnadoodhwala.com
+          </div>
+        </div>
+      </div>
+      <div className="footer-main__download">
+        <div className="footer-main--title">
+          Download APP
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default FooterMain
+export default FooterMain;
