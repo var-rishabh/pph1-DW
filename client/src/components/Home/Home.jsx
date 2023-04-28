bdf48b06381423676103fcf2b3bd793cf8a2b414
Shubham Aggarwal <89722744+Shubham-Aggarwal-1306@users.noreply.github.com>
2023-04-28 21:41:52 +0530

    Testimonial

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import "./Home.css";
 import BookTrail from './BookTrail';
 import MeetTeam from './MeetTeam';
+import Testimonial from './Testimonial';
 
 const Home = () => {
   return (
     <>
+      <Testimonial/>
       <MeetTeam/>
       <BookTrail/>
     </>
