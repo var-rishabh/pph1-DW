736220f2f2be9d4956327151d4cfcffbe2f9f30b
Shubham Aggarwal <89722744+Shubham-Aggarwal-1306@users.noreply.github.com>
2023-04-29 21:45:36 +0530

    Email Password Register and login and load user

--- client/src/components/Header/HeaderMain.jsx
+++ client/src/components/Header/HeaderMain.jsx
@@ -2,42 +2,52 @@ import React from 'react'
 import "./HeaderMain.css";
 import Icon from "../../Assets/Icon.png";
 import profileSample from "../../Assets/profileSample.jpg";
+import { useSelector } from 'react-redux';
 
 const HeaderMain = () => {
+  const { user, loading } = useSelector(state => state.userReducer);
   return (
     <>
-    <div className="header-main">
-      <div className="header-main__icon">
-        <img src={Icon} alt="icon" onClick={() => window.location.href = '/'} />
+      <div className="header-main">
+        <div className="header-main__icon">
+          <img src={Icon} alt="icon" onClick={() => window.location.href = '/'} />
+        </div>
+        <div className="header-main__menu">
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/product">Product</a>
+            </li>
+            <li>
+              <a href="/gallery">Gallery</a>
+            </li>
+            <li>
+              <a href="/process">Process</a>
+            </li>
+          </ul>
+        </div>
+        <div className="header-main__profile">
+          <a href="/trail" className='header-main__profile--button'>Book Trail</a>
+          {loading ? (
+            <div className="loaderButton">
+              <div className="loader"></div>
+            </div>
+          ) : user ? (
+            <a href="/profile">
+              <img src={profileSample} alt="profile" />
+            </a>) : (
+            <a href="/login" className='header-main__profile--button'>
+              Login
+            </a>)}
+        </div>
       </div>
-      <div className="header-main__menu">
-        <ul>
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/product">Product</a>
-          </li>
-          <li>
-            <a href="/gallery">Gallery</a>
-          </li>
-          <li>
-            <a href="/process">Process</a>
-          </li>
-        </ul>
-      </div>
-      <div className="header-main__profile">
-        <a href="/trail" className='header-main__profile--trail'>Book Trail</a>
-        <a href="/profile">
-          <img src={profileSample} alt="profile" />
-        </a>
-      </div>
-    </div>
-    {/* Cover the space taken by header */}
-    <div className="header-main__cover"></div>
+      {/* Cover the space taken by header */}
+      <div className="header-main__cover"></div>
     </>
   );
 }
--- client/src/components/Signup/Location.jsx
+++ client/src/components/Signup/Location.jsx
@@ -1,11 +1,47 @@
 import React from 'react';
 import "./Location.css";
+import { useLocation } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { registerWithEmail } from '../../Actions/User';
 
 const Location = () => {
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const [city, setCity] = React.useState('');
+  const [area, setArea] = React.useState('');
+  const state = location.state;
+  const handleSignup = (e) => {
+    e.preventDefault();
+    dispatch(registerWithEmail(state.email, state.password, city, area));
+  }
   return (
-    <>
-      Location
-    </>
+    <div className="location">
+      <div className="location__form--container">
+        <form className="auth__form">
+          <div className="auth__form--input">
+            <select value={city} onChange={(e) => setCity(e.target.value)}>
+              <option value="select" disabled>Select City</option>
+              <option value="Delhi">Delhi</option>
+              <option value="Mumbai">Mumbai</option>
+              <option value="Bangalore">Bangalore</option>
+              <option value="Chennai">Chennai</option>
+            </select>
+          </div>
+          <div className="auth__form--input">
+            <select value={area} onChange={(e) => setArea(e.target.value)}>
+              <option value="select" disabled>Select Area</option>
+              <option value="Delhi">Delhi</option>
+              <option value="Mumbai">Mumbai</option>
+              <option value="Bangalore">Bangalore</option>
+              <option value="Chennai">Chennai</option>
+            </select>
+          </div>
+          <div className="auth__form--button">
+            <button type="submit" onClick={handleSignup}>Sign up</button>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
