8ea94ac2de59dfd950a5d073c3bff506208bdcb4
var-rishabh <right.rishabh@gmail.com>
2023-05-13 01:36:51 +0530

    user routes setup

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,3 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
+const userController = require("../controllers/userController");
+const { checkAuth } = require("../middlewares/authMiddleware");
+
+router.post("/addUser", checkAuth, userController.addUser);
+router.get("/getAllUsers", userController.getAllUsers);
+
+module.exports = router;
