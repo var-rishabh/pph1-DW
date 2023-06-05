f6e6cf78a32d7ac03d58c8f4d7416f405f0ac1eb
Rishabh Varshney <right.rishabh@gmail.com>
2023-06-06 02:22:42 +0530

    transaction APIs completed

--- server/routes/walletRoutes.js
+++ server/routes/walletRoutes.js
@@ -7,5 +7,7 @@ const { checkAuth } = require("../middlewares/authMiddleware");
 router.get("/getbalance", checkAuth, walletController.getBalance);
 router.post("/checkout", checkAuth, walletController.checkout);
 router.post("/verification", walletController.verification);
+router.post("/userverification", checkAuth, walletController.userverification);
+router.get("/transactionhistory", checkAuth, walletController.transactionHistory);
 
 module.exports = router;
