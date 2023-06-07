dc6d4d48c26d28bcea4f37873a3dd9f0b08a0e17
Shubham Aggarwal <89722744+Shubham-Aggarwal-1306@users.noreply.github.com>
2023-06-08 00:46:24 +0530

    Wallet balance Fix

--- server/services/walletServices.js
+++ server/services/walletServices.js
@@ -3,11 +3,11 @@ const Transaction = require("../models/transactionModel");
 module.exports.getCurrentBalance = async (userID) => {
   var balance = 0;
   const transaction = await Transaction.findOne({
-    user_id: userID,
+    user_id: userID, payment_response: "success"
   }).sort({ _id: -1 });
-  if (transaction && transaction["payment_response"] === "success") {
+  if (transaction) {
     balance = transaction["balance"];
   }
-
+  console.log(transaction, balance);
   return balance;
 };
